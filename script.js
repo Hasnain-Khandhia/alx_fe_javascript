bf55f1e649371618ccf05613c1cef6a2a3030a9b
Hasnain-Khandhia <khandhiah@gmail.com>
2024-08-18 22:01:21 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Inspiration" },
+    { text: "Life is 10% what happens to us and 90% how we react to it.", category: "Life" },
+    { text: "Your time is limited, don't waste it living someone else's life.", category: "Motivation" }
+];
